ca0a8f8c6d53e22639f22f41f4041ed40725238f
Alisa Tran <a43tran@uwaterloo.ca>
2026-05-17 19:19:50 -0400

    Added more blocks, grass and sky

--- sketch.js
+++ sketch.js
@@ -12,20 +12,67 @@ function preload() {
 
 function setup() {
   createCanvas(800, 600);
-  background('#298499');
+  background('#BAEFFF');
+
+  fill('#6A9C52');
+  rect(0, height - height / 5, width, height / 5);
 
   textAlign(CENTER);
   textSize(14);
+  textStyle(BOLD);
   fill(0);
-  text("Meow, meow, meow, meow", width / 2, height / 2);
+  text("Meow, meow, meow, meow!", width / 2, height / 5);
 
-  image(catImg, height - 200, 85, catImg.width / 6, catImg.height / 6);
-
-  let originX = 280;
+  let originX = 250;
   let originY = 200;
 
+  // Jenga Blocks
   stroke('#45041A');
   strokeWeight(2);
   fill('#E882A6');
-  rect(originX, originY, 280, 50);
+  rect(originX - 30, originY, 280, 50, 5);
+
+  stroke('#1C3B08');
+  fill('#ABE882');
+  rect(originX - 10, originY + 50, 93, 50, 5);
+  
+  stroke('#080C3B');
+  fill('#646DD9');
+  rect(originX + 83, originY + 50, 93, 50, 5);
+
+  stroke('#301F05');
+  fill('#FCB149');
+  rect(originX + 176, originY + 50, 93, 50, 5);
+
+  stroke('#063040');
+  fill('#3DAED9');
+  rect(originX, originY + 100, 280, 50, 5);
+
+  stroke('#59530A');
+  fill('#FFF15C');
+  rect(originX, originY + 150, 93, 50, 5);
+  
+  stroke('#45041A');
+  fill('#E882A6');
+  rect(originX + 93, originY + 150, 93, 50, 5);
+
+  stroke('#080C3B');
+  fill('#646DD9');
+  rect(originX, originY + 200, 280, 50, 5);
+
+  stroke('#063040');
+  fill('#3DAED9');
+  rect(originX, originY + 250, 93, 50, 5);
+
+
+  stroke('#59530A');
+  fill('#FFF15C');
+  rect(originX + 186, originY + 250, 93, 50, 5);
+
+  stroke('#1C3B08');
+  fill('#ABE882');
+  rect(originX, originY + 300, 280, 50, 5);
+
+  image(catImg, height - 230, 85, catImg.width / 6, catImg.height / 6);
+
 }
