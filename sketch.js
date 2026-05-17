7190917fcadb1e0f7514574f1768982ee6b6bd48
Alisa Tran <a43tran@uwaterloo.ca>
2026-05-17 19:39:29 -0400

    Added bushes

--- sketch.js
+++ sketch.js
@@ -14,14 +14,28 @@ function setup() {
   createCanvas(800, 600);
   background('#BAEFFF');
 
-  fill('#6A9C52');
-  rect(0, height - height / 5, width, height / 5);
-
+  // Text Speech
   textAlign(CENTER);
   textSize(14);
   textStyle(BOLD);
   fill(0);
   text("Meow, meow, meow, meow!", width / 2, height / 5);
+
+  // Grass (background floor)
+  noStroke();
+  fill('#75962C');
+  rect(0, height - height / 5, width, height / 5);
+
+  // Bushes
+  fill('#A2C25B');
+  ellipse(0, 550, 300);
+  ellipse(150, 550, 180);
+  ellipse(width, 550, 210);
+
+  fill('#C1DB88');
+  ellipse(100, height, 160);
+  ellipse(0, height, 200);
+  ellipse(700, height, 200);
 
   let originX = 250;
   let originY = 200;
@@ -64,7 +78,6 @@ function setup() {
   fill('#3DAED9');
   rect(originX, originY + 250, 93, 50, 5);
 
-
   stroke('#59530A');
   fill('#FFF15C');
   rect(originX + 186, originY + 250, 93, 50, 5);
@@ -72,6 +85,14 @@ function setup() {
   stroke('#1C3B08');
   fill('#ABE882');
   rect(originX, originY + 300, 280, 50, 5);
+
+  stroke('#301F05');
+  fill('#FCB149');
+  rect(originX, originY + 350, 93, 50, 5);
+  
+  stroke('#45041A');
+  fill('#E882A6');
+  rect(originX + 186, originY + 350, 93, 50, 5);
 
   image(catImg, height - 230, 85, catImg.width / 6, catImg.height / 6);
 
